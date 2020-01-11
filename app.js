59302a7dd14e10171a541e0d8537f609fc83eed0
Chrys Propster <cpropster@gmail.com>
2020-01-11 09:24:21 -0800

    cloned down profs

--- app.js
+++ app.js
@@ -1,54 +1,98 @@
-const loadData = async () => {
-  const peoplePromise = axios
-    .get("http://star-cors.herokuapp.com/people")
-    .then((response) => response.data);
-  const filmsPromise = axios
-    .get("http://star-cors.herokuapp.com/films")
-    .then((response) => response.data);
-  const vehiclesPromise = axios
-    .get("http://star-cors.herokuapp.com/vehicles")
-    .then((response) => response.data);
-  const starshipsPromise = axios
-    .get("http://star-cors.herokuapp.com/starships")
-    .then((response) => response.data);
+const API = `http://star-cors.herokuapp.com`;
+const endpoints = ["people", "films", "vehicles", "starships"];
+const urls = endpoints.map((endpoint) => {
+  return `${API}/${endpoint}`;
+});
+const promises = urls.map((url) => {
+  return fetch(url).then((response) => response.json());
+});
 
-  const [people, films, vehicles, starships] = await Promise.all([
-    peoplePromise,
-    filmsPromise,
-    vehiclesPromise,
-    starshipsPromise,
-  ]);
+const renderData = (endpoint, data, itemRenderer) => {
+  console.log(data);
+  const div = document.querySelector(`#${endpoint}`);
+  let html = data.results
+    .map((item) => {
+      return `
+      <li>
+        ${itemRenderer(item)}
+      </li>
+    `;
+    })
+    .join("");
+  html = `<h2>${endpoint}</h2><input /><div>Viewing <span class='count'>${data.results.length}</span> of ${data.count}</div><ul>${html}</ul>`;
 
-  console.log(people);
-  console.log(films);
-  console.log(vehicles);
-  console.log(starships);
-
-  //is there any way to do this where you cycle through the items to create an array of items and insert where needed?
-  const renderPeople = (people) => {
-    const peopleElem = document.querySelector("#people");
-    let html = people.results
-      .map((person) => {
-        return `
-        <li class="list-group-item">
-        ${person.name}<br><br>Has appeared in ${person.films.length} films
-        </li>
-        `;
-      })
-      .join("");
-    html = `<h4>PEOPLE</h4><ul class="list-group">${html}</ul>`;
-    peopleElem.innerHTML = html;
-  };
-
-  const renderVehicles = (vehicles) => {};
-  const renderFilms = (films) => {};
-  const renderStarships = (starships) => {};
-
-  let filmsElem = document.querySelector("#films");
-  let vehiclesElem = document.querySelector("#vehicles");
-  let starshipsElem = document.querySelector("starships");
-
-  renderPeople(people);
+  div.innerHTML = html;
+  setUpSearch(div);
 };
 
-loadData();
+const setUpSearch = (div) => {
+  const input = div.querySelector("input");
+  const ul = div.querySelector("ul");
+  const counter = div.querySelector(".count");
+  input.addEventListener("keyup", (ev) => {
+    const search = ev.target.value;
+    const lis = [...ul.children];
+    let count = 0;
+    lis.forEach((li) => {
+      if (li.innerHTML.toLowerCase().includes(search.toLowerCase())) {
+        li.classList.remove("hide");
+        count++;
+      } else {
+        li.classList.add("hide");
+      }
+    });
+    counter.innerHTML = count;
+  });
+};
+
+const renderPeople = (people) => {
+  renderData("people", people, function(person) {
+    return `
+          The name is: ${person.name}
+          <br />
+          Has appeared in ${person.films.length} films.
+      `;
+  });
+};
+
+const renderFilms = (films) => {
+  renderData("films", films, (film) => {
+    return `
+        <b>${film.title}</b>
+        <br />
+        Released On ${
+          ["Su", "Mo", "Tues", "Wed", "Thurs", "Friday", "Sat"][
+            new Date(film.release_date).getDay()
+          ]
+        } ${film.release_date}
+      `;
+  });
+};
+
+const renderVehicles = (vehicles) => {
+  renderData("vehicles", vehicles, (vehicle) => {
+    return `
+        ${vehicle.name}
+        <br />
+        ${vehicle.manufacturer}
+      `;
+  });
+};
+
+const renderStarships = (starships) => {
+  renderData("starships", starships, (starship) => {
+    return `
+        ${starship.name}
+        <br />
+        ${starship.manufacturer}
+      `;
+  });
+};
+
+Promise.all(promises).then((result) => {
+  const [people, films, vehicles, starships] = result;
+  renderPeople(people);
+  renderFilms(films);
+  renderVehicles(vehicles);
+  renderStarships(starships);
+});
